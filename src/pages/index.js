c76c5f070b074abde46c83fbfc0b1367b81b7035
Jind0sh <josejr2498@gmail.com>
2022-07-14 07:01:13 +0900

    .

--- src/pages/index.js
+++ src/pages/index.js
@@ -30,7 +30,7 @@ export default function Home() {
   const {siteConfig} = useDocusaurusContext();
   return (
     <Layout
-      title={`${siteConfig.title}`}
+      title={Catatan Daring Yohn}
       description="Description will go into a meta tag in <head />">
       <HomepageHeader />
       <main>
