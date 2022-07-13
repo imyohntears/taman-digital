f1714c0093a27735e83e15f8b22c17327283c35f
Jind0sh <josejr2498@gmail.com>
2022-07-14 05:01:28 +0900

    initial commit

---
+++ docs/intro/intro.md
@@ -0,0 +1,40 @@
+---
+slug: /
+title: Taman Digital
+sidebar-position: 1
+---
+
+# Taman Digital 🌿
+
+Selamat datang. Ini sa punya wiki pribadi, tempat di mana sa prasastikan dan bagikan beragam hal yang sa tertarik untuk pelajari lebih lanjut dalam bentuk wiki online memakai Docusaurus di GitHub.
+
+
+## Ini apa?!!
+Anggap saja ini sa punya "otak digital", bookmark lebih canggih mungkin hahaha. Di taman ini nanti ada sa punya catatan-catatan, tulisan-tulisan sembarang, tautan-tautan menarik yg sa cukup peduli untuk pelajari yang kemudian bisa dibagikan. Sudah capek kehilangan *notes*.
+
+Setelah intro, setiap laman di *sidebar* diurut secara alfabetik. Nanti pasti akan beranak-cucu seperti boneka Rusia tapi jelajahi saja, mungkin ada sesuatu yang memikat mata.
+
+
+## Struktur Laman Konten
+Struktur itu penting, jadi kira-kira begini:
+
+1. Judul dengan deskripsi tentang topik
+2. Pandangan pribadi tentang topik, pertama baca di mana, dsb
+3. Subtopik - Beragam subtopik perihal topik utama
+4. Catatan - Catatan dan hal-hal menarik yang sa ketemu di internet mengenai topik. 
+5. Kode (kalau perlu) - Potongan kode, snippets
+6. Tautan - Tautan tentang topik
+
+
+## Baku bantu tingkatkan wiki
+Kalau ada hal yang sa tulis salah sekali ko mau banting keyboard, *fork* [*repository* ini](https://github.com) dan bikin *Push Request* untuk koreksi; atau [tambahkan *issue*](https://github.com) untuk tanya pertanyaan, tuliskan apa yang salah, atau beri saran. 
+
+
+## Bikin ko punya taman digital sendiri 🌱
+Wiki-wiki seperti ini diberi nama [taman digital](https://joelhooks.com/digital-garden). Ada [komunitas DigitalGardens di Reddit](https://www.reddit.com/r/DigitalGardens/) yang berfokus di pembahasan alat-alat untuk memulai/pelihara wiki-wiki seperti ini, dan juga cara menggunakan taman itu sebagai [alat/instrumen](https://numinous.productions/ttft/) dalam proses kognitif yang turut membenahi pikiran.
+
+Ini ajakan: sa harap yang datang di sini juga berniat bagikan apa yang mereka tau tentang dunia dengan cara yang bisa diakses orang lain.
+
+Sa aktif di [Twitter](https://twitter.com/jind0sh) , [Instagram](https://www.instagram.com/giyaibo/) dan [Telegram](https://t.me/harpokratez) 
+
+# Kasumasa
---
+++ docusaurus.config.js
@@ -0,0 +1,131 @@
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+
+const lightCodeTheme = require('prism-react-renderer/themes/github');
+const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: 'Taman Digital 🌿',
+  tagline: 'Jurnal digital milik Yose Giyay, di awan untuk dibagikan.',
+  url: 'https://your-docusaurus-test-site.com',
+  baseUrl: '/',
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'throw',
+  favicon: 'img/favicon.ico',
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: 'imyohntears', // Usually your GitHub org/user name.
+  projectName: 'taman-digital', // Usually your repo name.
+
+  // Even if you don't use internalization, you can use this field to set useful
+  // metadata like html lang. For example, if your site is Chinese, you may want
+  // to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en'],
+  },
+
+  presets: [
+    [
+      'classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          sidebarPath: require.resolve('./sidebars.js'),
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl:
+            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+        },
+        blog: {
+          showReadingTime: true,
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl:
+            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      navbar: {
+        title: 'Taman Digital',
+        logo: {
+          alt: 'My Site Logo',
+          src: 'img/logo.svg',
+        },
+        items: [
+          {
+            type: 'doc',
+            docId: 'intro/intro',
+            position: 'left',
+            label: 'Arsip',
+          },
+          {to: '/blog', label: 'Blog', position: 'left'},
+          {
+            href: 'https://instagram.com/giyaibo',
+            label: '📷',
+            position: 'right',
+          },
+          {
+            href: 'https://twitter.com/jind0sh',
+            label: '🐦',
+            position: 'right',
+          },
+          {
+            href: 'https://github.com/taman-digital/',
+            position: 'right',
+            className: "header-github-link",
+            "aria-label": "GitHub repository",
+          },          
+        ],
+      },
+      footer: {
+        style: 'light',
+        links: [
+          {
+            title: 'Docs',
+            items: [
+              {
+                label: 'Taman Digital',
+                to: '/docs',
+              },
+            ],
+          },
+          {
+            title: 'Kontak',
+            items: [
+              {
+                label: 'Twitter',
+                href: 'https://twitter.com/jind0sh',
+              },
+            ],
+          },
+          {
+            title: 'More',
+            items: [
+              {
+                label: 'GitHub',
+                href: 'https://github.com/facebook/docusaurus',
+              },
+            ],
+          },
+        ],
+        copyright: `Copyright © ${new Date().getFullYear()} Ebegu, Built with Docusaurus.`,
+      },
+      prism: {
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme,
+      },
+    }),
+};
+
+module.exports = config;
